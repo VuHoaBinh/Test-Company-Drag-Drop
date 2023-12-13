47f998744f745434424af307e0b3fe3883a6790a
VuHoaBinh <hoabinh.vippro63@gmail.com>
2023-12-13 12:02:59 +0700

    update file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { CdkDragEnd } from '@angular/cdk/drag-drop';
 
+// the structure of Box
 interface Box {
   id: number;
   text: string;
@@ -18,15 +19,18 @@ interface Box {
 })
 export class AppComponent implements OnInit {
   isCollapsed = false;
-  boxes: {
-    id: number;
-    text: string;
-    position: { x: number; y: number };
-    editing?: boolean;
-    size?: string;
-    font?: string;
-    color?: string;
-  }[] = [];
+
+  // boxes of Box
+  // boxes: {
+  //   id: number;
+  //   text: string;
+  //   position: { x: number; y: number };
+  //   editing?: boolean;
+  //   size?: string;
+  //   font?: string;
+  //   color?: string;
+  // }[] = [];
+  boxes: Box[] = [];
 
   ngOnInit(): void {
     const storedBoxes = localStorage.getItem('boxes');
@@ -35,7 +39,9 @@ export class AppComponent implements OnInit {
     }
   }
 
+  // get index
   onDragEnd(event: CdkDragEnd): void {
+    console.log(event);
     const draggedItem = event.source.data;
     const newPosition = event.source.getFreeDragPosition();
 
@@ -44,12 +50,15 @@ export class AppComponent implements OnInit {
     localStorage.setItem('boxes', JSON.stringify(this.boxes));
   }
 
+  // add box
+
   addBox(): void {
-    const newBox = {
+    const newBox: Box = {
       id: Date.now(),
       text: 'Drag me!',
       position: { x: 0, y: 0 },
     };
+
     this.boxes.push(newBox);
 
     localStorage.setItem('boxes', JSON.stringify(this.boxes));
@@ -61,6 +70,8 @@ export class AppComponent implements OnInit {
 
   //delete
   hoveredTextItem: Box | null = null;
+
+  //hover mouse
   onMouseOver(textItem: Box): void {
     this.hoveredTextItem = textItem;
   }
@@ -73,7 +84,7 @@ export class AppComponent implements OnInit {
     if (index !== null && index >= 0 && index < this.boxes.length) {
       this.boxes.splice(index, 1);
       localStorage.setItem('boxes', JSON.stringify(this.boxes));
-      this.editingIndex = null; // Assuming you want to exit editing mode
+      this.editingIndex = null;
     }
   }
 
@@ -82,11 +93,6 @@ export class AppComponent implements OnInit {
   stopEditing(): void {
     this.editingIndex = null;
     localStorage.setItem('boxes', JSON.stringify(this.boxes));
-  }
-
-  // feature :  làm việc với danh sách động *ngFor.
-  trackByFn(index: number, item: Box): number {
-    return item.id;
   }
 
   // set value h1 h2 h3 ...
